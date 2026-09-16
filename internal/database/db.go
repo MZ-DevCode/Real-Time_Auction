d22bacae71ffeb755aae0d89326853a697b6e03d
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-16 20:05:13 +0200

    table users

--- internal/database/db.go
+++ internal/database/db.go
@@ -5,20 +5,38 @@ import (
 	"log"
 )
 
-var db *sql.DB
+var DB *sql.DB
 
 func InitDB() {
 	var err error
 
-	db, err = sql.Open("sqlite", "auction.db")
+	DB, err = sql.Open("sqlite", "auction.db")
 	if err != nil {
 		log.Println("Ошибка открытия базы данных: ", err)
 		return
 	}
 
-	if err = db.Ping(); err != nil {
+	if err = DB.Ping(); err != nil {
 		log.Println("База данных не отвечает: ", err)
 	}
 
 	log.Println("Успешное подключение к базе данных")
+
+	createTables()
+}
+
+func createTables() {
+	users := `
+	CREATE TABLE IF NOT EXIST users(
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	username TEXT UNIQUE NOT NULL,
+	password_hash TEXT NOT NULL,
+	balance REAL DEFAULT 0,
+	created_at DATETIME
+	);
+	`
+	_, err := DB.Exec(users)
+	if err != nil {
+		log.Println("Ошибка при создании таблицы users: ", err)
+	}
 }
