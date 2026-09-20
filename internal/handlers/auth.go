2d9a1987d706700bde40137bd40ec7b3a2cef18c
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-20 19:53:14 +0200

    json struct

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -2,9 +2,15 @@ package handlers
 
 import "net/http"
 
+type RegisterRequest struct {
+	Name     string `json:"name"`
+	Username string `json:"name"`
+	Password string `json:"name"`
+}
+
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-
+	case "POST":
 	}
 }
 
