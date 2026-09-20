12cf84eb2f852e25b3373fd91f4103763fd34eed
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-20 19:56:03 +0200

    json decoder

--- internal/handlers/auth.go
+++ internal/handlers/auth.go
@@ -1,16 +1,25 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 type RegisterRequest struct {
 	Name     string `json:"name"`
-	Username string `json:"name"`
-	Password string `json:"name"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
+		var req RegisterRequest
+		err := json.NewDecoder(r.Body).Decode(&req)
+		if err != nil {
+			http.Error(w, "Bad request", http.StatusBadRequest)
+			return
+		}
 	}
 }
 
