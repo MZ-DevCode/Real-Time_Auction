64fc9e130656b0dbc32392225099c7bb9aef0c29
MZ-DevCode <248728007+MZ-DevCode@users.noreply.github.com>
2026-09-23 16:28:29 +0200

    fix

--- internal/handlers/lots.go
+++ internal/handlers/lots.go
@@ -15,30 +15,44 @@ func GetLotsHandlers(w http.ResponseWriter, r *http.Request) {
 		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 		defer cancel()
 
-		query := "SELECT id, title, description, price, time, status FROM lots"
-		rows, err := database.DB.QueryContext(ctx, query)
+		query := "SELECT id, title, description, start_price, current_price, ends_at, status FROM lots"		rows, err := database.DB.QueryContext(ctx, query)
 		if err != nil{
 			http.Error(w, "Ошибка при читании базы данных", http.StatusInternalServerError)
 			return
 		}
-	defer rows.Close()
-	for rows.Next() {
-		var (
-			id          int
-			title       string
-			description string
-			price       float64
-			time        int
-			status      string
-		)
 
 		var lots []models.Lot
 
-		lots, err := rows.Scan(&id, &title, &description, &price, &time, &status)
+		defer rows.Close()
+
+	for rows.Next() {
+		var (
+        	id           int64
+        	title        string
+   	    	description  string
+         	startPrice   float64
+         	currentPrice float64
+          	endsAt       time.Time
+      	    status       string
+		)
+
+		err := rows.Scan(&id, &title, &description, &price, &time, &status)
 		if err != nil {
 			http.Error(w, "Ошибка при чтении товаров из базы данных", http.StatusInternalServerError)
 			return
 		}
+
+		lot := models.Lot{
+    		ID:           id,
+      		Title:        title,
+        	Description:  description,
+         	StartPrice:   startPrice,
+    		CurrentPrice: currentPrice,
+      		EndsAt:       endsAt,
+        	Status:       status,
+		}
+
+		lots = append(lots, lot)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
